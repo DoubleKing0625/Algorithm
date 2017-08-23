c61d255f38144de30bff83f2161c7dcc6f8be0a3
DoubleKing <kevin.pqx@outlook.com>
2017-08-23 18:17:32 +0800

    Create 15_3sum.cpp

---
+++ 15_3sum.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Peng Qixiang on 2017/8/23.
+//
+/*
+// Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+// Find all unique triplets in the array which gives the sum of zero.
+// Input: S = [-1, 0, 1, 2, -1, -4],
+// Output: [[-1, 0, 1],[-1, -1, 2]]
+*/
+
+# include <cstdlib>
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+
+vector<vector<int>> minimumTotal(vector<int>& nums){
+    
+}
+
+int main() {
+
+    return 0;
+}
